3b4485761f688b38df05f831603f11db0345ac83
Thorzen <jthorsen@outlook.com>
2021-02-21 21:09:48 -0500

    Guess number bash script

---
+++ guess-number.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+min_num=1;
+max_num=100;
+num=$(($min_num + $RANDOM % $max_num)); #Rand number from min to max.
+re='^[0-9]+$';
+
+guess=-1
+attempts=0
+while [[ $num != $guess ]]; do
+    printf "Guess a number between %s and %s\n" $min_num $max_num
+    read -r guess;
+    if ! [[ $guess =~ $re ]]; then 
+        echo "Must be number"
+    elif [[ $guess == $num ]]; then
+        echo "YOU GUESSED RIGHT!"
+    elif [[ $guess -gt $num ]]; then 
+        echo "Guess Lower"
+        ((attempts++))
+    else 
+        echo "Guess Higher";
+        ((attempts++))
+    fi
+done
+
+printf "It took %s attempts\n" $attempts
